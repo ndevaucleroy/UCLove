abdd71b9363fe8667548b05b6b623874c1d35bd0
maxencewynen <maxence.wynen@student.uclouvain.be>
2016-04-28 17:05:54 +0200

    Create Album.java

---
+++ app/src/main/java/lsinf/uclove/Album.java
@@ -0,0 +1,26 @@
+package lsinf1225.uclove;
+
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Album here.
+ *
+ * @author Groupe P
+ * @version 25.04.2016
+ */
+public class Album
+{
+    // instance variables - replace the example below with your own
+    private ArrayList album;
+
+    /**
+     * Constructor for objects of class Album
+     */
+    public Album(ArrayList album)
+    {
+        // initialise instance variables
+        this.album = album;
+    }
+
+
+}
