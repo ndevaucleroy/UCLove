3d5af80f32a77e34e146e5dfe24866caead12241
ndevaucleroy <nicolas.devaucleroy@student.uclouvain.be>
2016-04-29 11:53:41 +0200

    Update User.java

--- app/src/main/java/lsinf/uclove/User.java
+++ app/src/main/java/lsinf/uclove/User.java
@@ -9,20 +9,19 @@ import java.util.ArrayList;
  */
 public class User
 {
-    protected String password;
-    private ArrayList friendsList;
+    private String password;
     private String loginStr, firstNameStr, nameStr, placeStr, birthdayStr, languageStr,
             hairStr, eyesStr, descriptionStr, genderStr, orientationStr;
+    private Friends friends;
 
     /**
      * Constructor for objects of class User
      */
-    public User(String password, ArrayList friendsList, String loginStr, String  firstNameStr,
+    public User(String password, String loginStr, String  firstNameStr,
                 String  nameStr, String  placeStr, String birthdayStr, String languageStr,
                 String hairStr, String  eyesStr, String descriptionStr, String genderStr, String  orientationStr)
     {
         this.password = password;
-        this.friendsList = friendsList;
         this.loginStr = loginStr;
         this.firstNameStr = firstNameStr;
         this.nameStr = nameStr;
@@ -34,43 +33,42 @@ public class User
         this.descriptionStr = descriptionStr;
         this.genderStr = genderStr;
         this.orientationStr = orientationStr;
+        this.friends = new Friends(this.loginStr, true);
+        DatabaseHandler.createUser(this);
     }
 
-    public User(String login){
+    public User(){
+    }
+    public User(String loginStr) {
+        this.loginStr = loginStr;
+        this.password = DatabaseHandler.getPassword(loginStr);
+        this.firstNameStr = DatabaseHandler.getFisrtNameStr(loginStr);
+        this.nameStr = DatabaseHandler.getNameStr(loginStr);
+        this.placeStr = DatabaseHandler.getPlaceStr(loginStr);
+        this.birthdayStr = DatabaseHandler.getBirthdayStr(loginStr);
+        this.languageStr = DatabaseHandler.getLanguageStr(loginStr);
+        this.hairStr = DatabaseHandler.getHairStr(loginStr);
+        this.eyesStr = DatabaseHandler.getEyesStr(loginStr);
+        this.descriptionStr = DatabaseHandler.getDescriptionStr(loginStr);
+        this.genderStr = DatabaseHandler.getGenderStr(loginStr);
+        this.orientationStr = DatabaseHandler.getOrientation(loginStr);
+        this.friends = new Friends(loginStr, false);
     }
 
     public static boolean isLoginAvailable(String username){
         return true;
     }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ArrayList getFriendsList() { return friendsList;}
-
-    public void setFriendList(ArrayList friendsList) {
-        this.friendsList = friendsList;
-    }
-
-    public String getLoginStr() {
+    public String getLoginStr(){
         return loginStr;
     }
-
-    public void setLoginStr(String loginStr) {
-        this.loginStr = loginStr;
-    }
-
+    
     public String getFirstNameStr() {
         return firstNameStr;
     }
 
     public void setFirstNameStr(String firstNameStr) {
         this.firstNameStr = firstNameStr;
+        DatabaseHandler.setFirstName(firstNameStr, loginStr);
     }
 
     public String getNameStr() {
@@ -79,6 +77,7 @@ public class User
 
     public void setNameStr(String nameStr) {
         this.nameStr = nameStr;
+        DatabaseHandler.setNameStr(nameStr, loginStr);
     }
 
     public String getPlaceStr() {
@@ -87,6 +86,7 @@ public class User
 
     public void setPlaceStr(String placeStr) {
         this.placeStr = placeStr;
+        DatabaseHandler.setPlaceStr(placeStr, loginStr);
     }
 
     public String getBirthdayStr() {
@@ -95,6 +95,7 @@ public class User
 
     public void setBirthdayStr(String birthdayStr) {
         this.birthdayStr = birthdayStr;
+        DatabaseHandler.setBirthdayStr(birthdayStr, loginStr);
     }
 
     public String getLanguageStr() {
@@ -103,6 +104,7 @@ public class User
 
     public void setLanguageStr(String languageStr) {
         this.languageStr = languageStr;
+        DatabaseHandler.setLanguageStr(languageStr, loginStr);
     }
 
     public String getHairStr() {
@@ -111,6 +113,7 @@ public class User
 
     public void setHairStr(String hairStr) {
         this.hairStr = hairStr;
+        DatabaseHandler.setHairStr(hairStr, loginStr);
     }
 
     public String getEyesStr() {
@@ -119,6 +122,7 @@ public class User
 
     public void setEyesStr(String eyesStr) {
         this.eyesStr = eyesStr;
+        DatabaseHandler.setEyesStr(eyesStr, loginStr);
     }
 
     public String getDescriptionStr() {
@@ -127,6 +131,7 @@ public class User
 
     public void setDescriptionStr(String descriptionStr) {
         this.descriptionStr = descriptionStr;
+        DatabaseHandler.setDescription(description, loginStr);
     }
 
     public String getGenderStr() {
@@ -135,6 +140,7 @@ public class User
 
     public void setGenderStr(String genderStr) {
         this.genderStr = genderStr;
+        DatabaseHandler.setGenderStr(genderStr, loginStr);
     }
 
     public String getOrientationStr() {
@@ -143,5 +149,10 @@ public class User
 
     public void setOrientationStr(String orientationStr) {
         this.orientationStr = orientationStr;
+        DatabaseHandler.setOrientationStr(orientationStr, loginStr);
+    }
+
+    public Friends getFriends() {
+        return friends;
     }
 }
