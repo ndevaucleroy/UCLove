02e453ca20f11148302196861bbd939f67faa735
ndevaucleroy <nicolas.devaucleroy@student.uclouvain.be>
2016-04-29 14:04:41 +0200

    Update User.java

--- app/src/main/java/lsinf/uclove/User.java
+++ app/src/main/java/lsinf/uclove/User.java
@@ -4,24 +4,29 @@ import java.util.ArrayList;
 /**
  *
  *
- * @author: Groupe P 
+ * @author: Groupe P
  * @version: 25.04.2016
  */
 public class User
 {
-    private String password;
+    private String passwordStr;
     private String loginStr, firstNameStr, nameStr, placeStr, birthdayStr, languageStr,
             hairStr, eyesStr, descriptionStr, genderStr, orientationStr;
     private Friends friends;
 
+    boolean[] hair;
+    boolean[] eyes;
+    boolean samePlace;
+    int ageMin, ageMax;
     /**
      * Constructor for objects of class User
      */
-    public User(String password, String loginStr, String  firstNameStr,
+    public User(String passwordStr, String loginStr, String  firstNameStr,
                 String  nameStr, String  placeStr, String birthdayStr, String languageStr,
-                String hairStr, String  eyesStr, String descriptionStr, String genderStr, String  orientationStr)
+                String hairStr, String  eyesStr, String descriptionStr, String genderStr, String  orientationStr, boolean[] hair, boolean[] eyes, boolean samePlace,
+                int ageMin, int ageMax)
     {
-        this.password = password;
+        this.passwordStr = passwordStr;
         this.loginStr = loginStr;
         this.firstNameStr = firstNameStr;
         this.nameStr = nameStr;
@@ -34,6 +39,11 @@ public class User
         this.genderStr = genderStr;
         this.orientationStr = orientationStr;
         this.friends = new Friends(this.loginStr, true);
+        this.hair = hair;
+        this.eyes = eyes;
+        this.samePlace = samePlace;
+        this.ageMin = ageMin;
+        this.ageMax = ageMax;
         DatabaseHandler.createUser(this);
     }
 
@@ -55,25 +65,69 @@ public class User
         this.friends = new Friends(loginStr, false);
     }
 
+    public void setHair(boolean[] hair) {
+        this.hair = hair;
+    }
+
+    public void setEyes(boolean[] eyes) {
+        this.eyes = eyes;
+    }
+
+    public void setSamePlace(boolean samePlace) {
+        this.samePlace = samePlace;
+    }
+
+    public void setAgeMin(int ageMin) {
+        this.ageMin = ageMin;
+    }
+
+    public void setAgeMax(int ageMax) {
+        this.ageMax = ageMax;
+    }
+
+    public void setFriends(Friends friends) {
+        this.friends = friends;
+    }
+
+    public boolean[] getHair() {
+        return hair;
+    }
+
+    public boolean[] getEyes() {
+        return eyes;
+    }
+
+    public boolean getSamePlace() {
+        return samePlace;
+    }
+
+    public int getAgeMin() {
+        return ageMin;
+    }
+
+    public int getAgeMax() {
+        return ageMax;
+    }
+
     public static boolean isLoginAvailable(String username){
         return true;
     }
-    
+
     public String getLoginStr(){
         return loginStr;
     }
-    
+
     public void setLoginStr(String loginStr){
         this.loginStr = loginStr;
     }
     public String getPasswordStr(){
         return passwordStr;
     }
-    
-    public void setPasswordStr(String password){
-        this.password = password;
+
+    public void setPasswordStr(String passwordStr){
+        this.passwordStr = passwordStr;
     }
-    
+
     public String getFirstNameStr() {
         return firstNameStr;
     }
@@ -136,7 +190,7 @@ public class User
 
     public void setDescriptionStr(String descriptionStr) {
         this.descriptionStr = descriptionStr;
-    
+
     }
 
     public String getGenderStr() {
@@ -145,7 +199,7 @@ public class User
 
     public void setGenderStr(String genderStr) {
         this.genderStr = genderStr;
-      
+
     }
 
     public String getOrientationStr() {
@@ -154,10 +208,11 @@ public class User
 
     public void setOrientationStr(String orientationStr) {
         this.orientationStr = orientationStr;
-       
+
     }
 
     public Friends getFriends() {
         return friends;
     }
+
 }
