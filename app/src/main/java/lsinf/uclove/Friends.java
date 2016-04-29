b4b52b209f203f85d7f4b3c868afc646fff695da
ndevaucleroy <nicolas.devaucleroy@student.uclouvain.be>
2016-04-29 11:30:23 +0200

    Update Friends.java

--- app/src/main/java/lsinf/uclove/Friends.java
+++ app/src/main/java/lsinf/uclove/Friends.java
@@ -1,5 +1,8 @@
 package lsinf1225.uclove;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
  * Write a description of class Friends here.
@@ -10,21 +13,79 @@ package lsinf1225.uclove;
 public class Friends
 {
     // instance variables
-    private User friend;
-    private User sendFriendRequest;
-    private User receivedFriendRequest;
+    private String listOwnerStr;
+    private ArrayList<String> friendsListStr;
+    private ArrayList<String> recFriendsRequestsStr;
+    private ArrayList<String> sentFriendsRequestsStr;
+    //private User[] friendsListUsr;  //on veut qu'il se delete des qu'on l'utilise plus, car des users ca prend de la mémoire
     /*private Chat friendChat;*/
 
     /**
      * Constructor for objects of class Friends
      */
-    public Friends(User friend, User sendFriendRequest, User receivedFriendRequest /*,Chat friendChat*/)
-    {
-        this.friend = friend;
-        this.sendFriendRequest = sendFriendRequest;
-        this.receivedFriendRequest = receivedFriendRequest;
+
+    public Friends(String listOwnerStr, boolean alreadyCreated){
+     if(alreadyCreated){
+         this.listOwnerStr = listOwnerStr;
+         this.friendsListStr = null;
+         this.recFriendsRequestsStr = null;
+         this.recFriendsRequestsStr = null;
+     }
+     else{
+         this.listOwnerStr = listOwnerStr;
+         this.friendsListStr= new ArrayList<String>(Arrays.asList(DatabaseHandler.getFriends(listOwnerStr)));
+         this.recFriendsRequestsStr= new ArrayList<String>(Arrays.asList(DatabaseHandler.getRecFriendsRequests(listOwnerStr)));
+         this.sentFriendsRequestsStr = new ArrayList<String>(Arrays.asList(DatabaseHandler.getRecFriendsRequests(listOwnerStr)));
+     }
+
+    }
+    public Friends(){}
+
+    public boolean sendFriendRequest(String target){ //sends true if worked, sends false if it was already a friend
+        if(sentFriendsRequestsStr.contains(target)){
+        return false;
+        }
+        sentFriendsRequestsStr.add(target);
+        DatabaseHandler.addFriendRequest(listOwnerStr, target);
+        return true;
+    }
+    public void acceptFriendRequest(String target){
+        recFriendsRequestsStr.remove(target);
+        friendsListStr.add(target);
+        DatabaseHandler.addFriend(listOwnerStr, target);
+
+    }
+    public void refuseFriendRequest(String target){
+        recFriendsRequestsStr.remove(target);
+        DatabaseHandler.removeFriendRequest(listOwnerStr, target);
+    }
+    public void deleteFriend(String target){
+        friendsListStr.remove(target);
+        DatabaseHandler.removeFriend(listOwnerStr, target);
+    }
+    public String getListOwnerStr() {
+        return listOwnerStr;
     }
 
-    public Friends(){}
+    public String[] getFriendsListStr() {
+        return (String[])friendsListStr.toArray();
+    }
+
+    public String[] getRecFriendsRequestsStr() {
+        return (String[])recFriendsRequestsStr.toArray();
+    }
+
+    public String[] getSentFriendsRequestsStr() {
+        return (String[])sentFriendsRequestsStr.toArray();
+    }
+
+    public User[] getFriendsListUsr() { //on load les users
+        User []friendsListUsr = new User[friendsListStr.size()];
+        for(int i = 0;i < friendsListStr.size();i++){
+            friendsListUsr[i] = new User(friendsListStr.get(i));
+        }
+        return friendsListUsr;
+    }
+
 
 }
